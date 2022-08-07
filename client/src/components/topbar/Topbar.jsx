ad7a507294d383362c5a5f76af92978cb444a3e8
Napatsara <nigawa75@gmail.com>
2022-08-07 12:30:15 +0900

    first commit

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,46 @@
+import "./topbar.css"
+import {Link } from "react-router-dom";
+
+export default function TTopbar() {
+    const user = false;
+    return(
+        <div className="top">
+             <div className="topLeft">
+             <i className=" topIcon fa-brands fa-square-facebook"></i>
+             <i className=" topIcon fa-brands fa-square-twitter"></i>
+             <i className=" topIcon fa-brands fa-square-pinterest"></i>
+             <i className=" topIcon fa-brands fa-square-instagram"></i>
+             </div>
+             <div className="topCenter">
+                <ul className="topList">
+                <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+                <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+                <li className="topListItem"><Link className="link" to="/">CONTACT</Link></li>
+                <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+                <li className="topListItem">
+                    {user && "LOGOUT"}
+                </li>
+                </ul>
+             </div>
+             <div className="topRight">
+                {
+                    user ? (
+                        <img
+                          className="topImg"
+                          src="https://cdn.pixabay.com/photo/2017/03/26/21/54/yoga-2176668_960_720.jpg" 
+                          alt="" 
+                        />
+                    ) : (
+                    <ul className="topList">
+                      <li className="topListItem"><Link className="link" to="/login">LOGIN</Link></li>
+                      <li className="topListItem"><Link className="link" to="/register">REGISTER</Link></li>
+                    </ul>
+                  )
+
+                 }
+                
+                <i className=" topSearchIcon fa-solid fa-magnifying-glass"></i>
+             </div>
+        </div>
+    )
+}
